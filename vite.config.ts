bfd09da1aff01a82f228b6b7cd2fa7f12243b159
pashaarshad <arshadpashaintern@gmail.com>
2026-05-24 13:39:50 +0530

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { tanstackStart } from "@tanstack/react-start/plugin";
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
+    cloudflare({
+      viteEnvironment: { name: "ssr" },
+    }),
     tanstackStart({
       server: { entry: "server" },
     }),
-    viteReact(),
-    cloudflare(),
+    react(),
   ],
   resolve: {
     dedupe: ["react", "react-dom", "@tanstack/react-router"],
