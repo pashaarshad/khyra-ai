db2870e80e7dbc4100bc0e03f09d9d5d768ebcdc
pashaarshad <arshadpashaintern@gmail.com>
2026-05-24 20:38:03 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -13,7 +13,7 @@ export default defineConfig({
       server: { entry: "server" },
     }),
     react(),
-    netlify(),
+    nitro(),
   ],
   resolve: {
     dedupe: ["react", "react-dom", "@tanstack/react-router"],
